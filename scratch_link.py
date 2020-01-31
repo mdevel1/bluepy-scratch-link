2579b702a167d0a9b9b79965bc34fdd01a5d80ac
MDE <59321583+mdevel1@users.noreply.github.com>
2020-02-01 00:20:36 +0100

    BLESession.setNotifications: Refactored Start/Stop a little into a third fn

--- scratch_link.py
+++ scratch_link.py
@@ -344,8 +344,7 @@ class BLESession(Session):
         logger.debug(res)
         return res
 
-    def startNotifications(self, service_id, chara_id):
-        logger.debug(f"start notification for {chara_id}")
+    def setNotifications(self, service_id, chara_id, value):
         service = self.perip.getServiceByUUID(UUID(service_id))
         chas = service.getCharacteristics(forUUID=chara_id)
         handle = chas[0].getHandle()
@@ -357,18 +356,15 @@ class BLESession(Session):
                                        b"\x01\x00", True)
         self.lock.release()
 
+    def startNotifications(self, service_id, chara_id):
+        logger.debug(f"start notification for {chara_id}")
+        value = b"\x01\x00"
+        self.setNotifications(service_id=service_id, chara_id=chara_id, value=value)
+
     def stopNotifications(self, service_id, chara_id):
         logger.debug(f"stop notification for {chara_id}")
-        service = self.perip.getServiceByUUID(UUID(service_id))
-        chas = service.getCharacteristics(forUUID=chara_id)
-        handle = chas[0].getHandle()
-        # prepare notification handler
-        self.delegate.add_handle(service_id, chara_id, handle)
-        # request notification to the BLE device
-        self.lock.acquire()
-        self.perip.writeCharacteristic(chas[0].getHandle() + 1,
-                                       b"\x00\x00", True)
-        self.lock.release()
+        value = b"\x00\x00"
+        self.setNotifications(service_id=service_id, chara_id=chara_id, value=value)
 
     def end_request(self):
         logger.debug("end_request of BLESession")
